90f80ee5ccd733bfb217c3ffefc3d66d22cba6f3
Ahmed Sabir <43648923+ahmedssabir@users.noreply.github.com>
2023-02-24 12:13:05 +0100

    Add files via upload

--- BERT-CNN/eval.py
+++ BERT-CNN/eval.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 import sys
+import argparse
 from sklearn.model_selection import train_test_split
 
 sys.path.insert(0, "bert_experimental")
@@ -10,8 +11,16 @@ from bert_experimental.finetuning.text_preprocessing import build_preprocessor
 from bert_experimental.finetuning.graph_ops import load_graph
 
 
-df = pd.read_csv("test.tsv", sep='\t')
 
+parser=argparse.ArgumentParser(description='inference of the model')
+parser.add_argument('--testset',  default='test.tsv', help='test file', type=str,required=True) 
+parser.add_argument('--model', default='pre-trained model', help='', type=str, required=True) 
+args = parser.parse_args()
+
+
+
+df = pd.read_csv(args.testset, sep='\t')
+ 
 
 texts = []
 delimiter = " ||| "
@@ -25,7 +34,7 @@ texts = np.array(texts)
 trX, tsX = train_test_split(texts, shuffle=False, test_size=0.01)
 
 
-restored_graph = load_graph("frozen_graph.pb")
+restored_graph = load_graph(args.model)
 
 graph_ops = restored_graph.get_operations()
 input_op, output_op = graph_ops[0].name, graph_ops[-1].name
@@ -34,7 +43,10 @@ print(input_op, output_op)
 x = restored_graph.get_tensor_by_name(input_op + ':0')
 y = restored_graph.get_tensor_by_name(output_op + ':0')
 
-preprocessor = build_preprocessor("/Bert/uncased_L-12_H-768_A-12/vocab.txt", 64)
+# also need a flag here
+
+#
+preprocessor = build_preprocessor("uncased_L-12_H-768_A-12/vocab.txt", 64)
 py_func = tf.numpy_function(preprocessor, [x], [tf.int32, tf.int32, tf.int32], name='preprocessor')
 
 py_func = tf.numpy_function(preprocessor, [x], [tf.int32, tf.int32, tf.int32])
@@ -49,9 +61,8 @@ y = tf.print(y, summarize=-1)
 #x = tf.print(x, summarize=-1)
 y_out = sess.run(y, feed_dict={
         x: trX[:4].reshape((-1,1))
- 	#x: trX[:90000].reshape((-1,1))
+ 	      #x: trX[:90000].reshape((-1,1))
     })
-
 
 print(y_out)
 
